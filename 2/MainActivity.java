89a51f7ab0fdc564f007b0c1ce70bab0dc2917d5
dvaisluk <70915114+dvaisluk@users.noreply.github.com>
2021-11-25 21:12:31 +0300

    Add files via upload

---
+++ 2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.if2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickOpenWebpageButton(View view) {
+        dialPhoneNumber("89962395602");
+    }
+    public void dialPhoneNumber(String phoneNumber) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+}
